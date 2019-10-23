8e372324cab785a451a86f3dced5d6ed6a0d50ed
DESKTOP-LV38J50\ADMIN <lufeng@mankuhome.com>
2019-10-23 17:15:26 +0800

    基础代码

---
+++ Source/EventUtility/EventManager.cpp
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EventManager.h"
+#include "Engine.h"
+
+TMap<FString, TArray<UObject*>> UEventManager::AllListener;
+
+void UEventManager::AddEventListener(FString EventName, UObject* Listener)
+{
+	if (EventName.IsEmpty() || Listener == nullptr) {
+		return;
+	}
+
+	Listener->AddToRoot();
+
+	if (UEventManager::AllListener.Contains(EventName)) {
+		UEventManager::AllListener[EventName].Emplace(Listener);
+	}
+	else {
+		TArray<UObject*> arrTmp = { Listener };
+		UEventManager::AllListener.Emplace(EventName, arrTmp);
+	}
+}
+
+void UEventManager::RemoveEventListener(FString EventName, UObject* Listener)
+{
+	if (UEventManager::AllListener.Contains(EventName)) {
+		UEventManager::AllListener[EventName].Remove(Listener);
+		Listener->RemoveFromRoot();
+	}
+}
+
+UObject* UEventManager::NewAsset(UClass* ClassType)
+{
+	UObject* obj = NewObject<UObject>(GetTransientPackage(), ClassType);
+	if (obj) {
+		obj->AddToRoot();
+	}
+	return obj;
+}
+
+FString UEventManager::DispatchEvent(FString EventName, UObject* Datas)
+{
+	FString errorInfo = TEXT("");
+	if (EventName.IsEmpty()) errorInfo += "EventName Is Empty!";
+	if (Datas == nullptr) errorInfo += "Datas is nullptr!";
+	if (EventName.IsEmpty() || Datas == nullptr) {
+		if (Datas != nullptr) Datas->RemoveFromRoot();
+		return errorInfo;
+	}
+
+	if (UEventManager::AllListener.Contains(EventName) && UEventManager::AllListener[EventName].Num() > 0) {		
+		for (UObject*& obj : UEventManager::AllListener[EventName]) {
+			UFunction* fun = obj->FindFunction("ExecuteFun");
+			if (fun == nullptr) {
+				errorInfo += "'" + obj->GetName() + "'No 'ExecuteFun' Function. \n";
+			}
+			else {
+				obj->ProcessEvent(fun, &Datas);
+			}
+		}
+	}
+	else {
+		errorInfo = "'" + EventName + "'No Listener.";
+	}
+	Datas->RemoveFromRoot();
+	return errorInfo;
+}
